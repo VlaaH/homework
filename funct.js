6da4050e6a901b58f9b1212cd9025399f18f6cbd
Vlad <vlagolod@gmail.com>
2022-09-27 12:17:05 +0300

    arrayHomeWork

---
+++ funct.js
@@ -0,0 +1,36 @@
+
+// const arr = [1, 2, 'Vlad']
+// for (let i of arr){
+//     console.log(i)
+// 
+// for (let i=0; i < 3; i++){
+//     console.log(arr[i])
+// Масиви
+
+// ---------------------------------------
+// let test = 0
+// while (test < 10) {
+//     console.log(test)
+//     test += 1
+// }
+// Цикли
+
+// -----------------------------------
+// const obj = {
+//     first: 1,
+//     second: 2,
+//     third: 3,
+//     value: 'Vlad'
+// };
+// // Обєкти
+
+// for (let i in obj) {
+//     console.log(i)
+// }
+
+//---------------------------------------
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("Vlaaaaaadddddddd"));
